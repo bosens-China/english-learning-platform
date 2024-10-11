5e63abe454f9a1f74b70729964f0da952b871724
yliu <yangboses@gmail.com>
2024-10-11 09:31:34 +0800

    ci: 修复basePath缺少/前缀

--- packages/client-web/next.config.mjs
+++ packages/client-web/next.config.mjs
@@ -1,7 +1,7 @@
 /*
  * CI 输出目标不同
  */
-const basePath = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/').at(1) : undefined;
+const basePath = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/').at(1)}` : undefined;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
